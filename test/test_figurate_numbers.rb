0e34b43020413adc2a54504948797b7d1efbc54d
Edgar Delgado Vega <edelve91@gmail.com>
2024-06-30 17:59:52 -0500

    test: add unit test for stern prime numbers

--- test/test_figurate_numbers.rb
+++ test/test_figurate_numbers.rb
@@ -481,4 +481,7 @@ class TestFigurateNumbers  < Minitest::Test
     assert_equal([561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341, 41041, 46657, 52633, 62745, 63973, 75361, 101101, 115921, 126217, 162401, 172081, 188461, 252601, 278545, 294409, 314821, 334153, 340561, 399001, 410041, 449065, 488881, 512461, 530881, 552721], FigurateNumbers.carmichael_numbers.take(35))
   end
 
+  def test_stern_prime_numbers
+    assert_equal([2, 3, 17, 137, 227, 977, 1187, 1493], FigurateNumbers.stern_prime_numbers.take(8))
+  end
 end
