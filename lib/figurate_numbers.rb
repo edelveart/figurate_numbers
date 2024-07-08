e94e597b28b73e29bba5138d6951c453de40d284
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-08 04:04:55 -0500

    feat: add tetrahedral square pyramidal number

--- lib/figurate_numbers.rb
+++ lib/figurate_numbers.rb
@@ -901,6 +901,15 @@ module FigurateNumbers
     end
   end
 
+  def tetrahedral_square_pyramidal_number
+    Enumerator.new do |y|
+      finite_set = [1]
+      (finite_set).each do |delta|
+          y << delta
+      end
+    end
+  end
+
   def cubic_numbers
     Enumerator.new do |y|
       (1..Float::INFINITY).each do |delta|
