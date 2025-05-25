bf4d73664f896d0860d1740c4a586af431a8cc55
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-25 18:20:59 -0500

    feat(p-adic-utils): add padicExpansion function; refactor padicValuation and padicNorm

--- lib/figurate_numbers/utils/padic_utils.rb
+++ lib/figurate_numbers/utils/padic_utils.rb
@@ -6,7 +6,7 @@ module FigurateNumbers
   module PAdicUtils
     extend self
 
-    def p_adic_valuation(base_ten_number, p)
+    def padic_valuation(base_ten_number, p)
       raise "#{p} is not a prime number" unless Prime.prime?(p)
 
       index = 0
@@ -18,11 +18,37 @@ module FigurateNumbers
       index - 1
     end
 
-    def p_adic_norm(base_ten_number, p)
+    def padic_norm(base_ten_number, p)
       return 0 if base_ten_number == 0 # rubocop:disable Style/NumericPredicate
 
-      power = p_adic_valuation(base_ten_number, p)
-      1.0 / (p**power)
+      power = padic_valuation(base_ten_number, p)
+      Rational(1, p**power)
+    end
+
+    def padic_expansion(base_ten_number, p, precision = 11, music_reverse: false) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      inverse_limit_arr = []
+      (1...precision).each do |index|
+        power = p**index
+        inverse_limits = ((base_ten_number % power) + power) % power
+        inverse_limit_arr << inverse_limits
+      end
+      p_adic_expansion_array = [inverse_limit_arr[0]]
+
+      (0...(precision - 2)).each do |index|
+        a1 = inverse_limit_arr[index + 1]
+        a0 = inverse_limit_arr[index]
+        power = p**(index + 1)
+        digits = (a1 - a0) / power
+        p_adic_expansion_array << digits
+      end
+
+      if music_reverse
+        reversed = p_adic_expansion_array.reverse
+        first_nonzero_index = reversed.index { |digit| digit != 0 } || 0
+        reversed[first_nonzero_index..-1]
+      else
+        p_adic_expansion_array
+      end
     end
   end
 end
