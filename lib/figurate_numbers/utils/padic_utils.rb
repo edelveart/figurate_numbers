9a2d8736fdb27e65019ecf6151d65b8a4b768160
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-25 17:47:36 -0500

    feat(p-adic-utils): add p-adic norm method

--- lib/figurate_numbers/utils/padic_utils.rb
+++ lib/figurate_numbers/utils/padic_utils.rb
@@ -17,5 +17,12 @@ module FigurateNumbers
       end
       index - 1
     end
+
+    def p_adic_norm(base_ten_number, p)
+      return 0 if base_ten_number == 0 # rubocop:disable Style/NumericPredicate
+
+      power = p_adic_valuation(base_ten_number, p)
+      1.0 / (p**power)
+    end
   end
 end
