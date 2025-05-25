3e99934fbcd20d6e12864b16f5a07fcbdd2e0749
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-25 02:17:43 -0500

    refactor(figurate_numbers): extend submodules methods and define top-level aliases

--- lib/figurate_numbers.rb
+++ lib/figurate_numbers.rb
@@ -4,13 +4,11 @@ require_relative 'figurate_numbers/space_figurate_numbers'
 require_relative 'figurate_numbers/multidimensional_figurate_numbers'
 
 module FigurateNumbers
-
-  include PlaneFigurateNumbers
-  include SpaceFigurateNumbers
-  include MultiDimensionalFigurateNumbers
-
   extend PlaneFigurateNumbers
   extend SpaceFigurateNumbers
   extend MultiDimensionalFigurateNumbers
-
 end
+
+PlaneFigurateNumbers = FigurateNumbers::PlaneFigurateNumbers
+SpaceFigurateNumbers = FigurateNumbers::SpaceFigurateNumbers
+MultiDimensionalFigurateNumbers = FigurateNumbers::MultiDimensionalFigurateNumbers
