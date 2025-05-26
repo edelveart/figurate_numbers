1270e51afaf4eff0cb49e6743a05fe27699cd35f
Edgar Delgado Vega <edelve91@gmail.com>
2025-05-25 21:43:30 -0500

    feat(figurate-numbers): expose ArithFn module

--- lib/figurate_numbers.rb
+++ lib/figurate_numbers.rb
@@ -2,13 +2,16 @@ require_relative 'figurate_numbers/version'
 require_relative 'figurate_numbers/plane_figurate_numbers'
 require_relative 'figurate_numbers/space_figurate_numbers'
 require_relative 'figurate_numbers/multidimensional_figurate_numbers'
+require_relative 'figurate_numbers/arithfn'
 
 module FigurateNumbers
   extend PlaneFigurateNumbers
   extend SpaceFigurateNumbers
   extend MultiDimensionalFigurateNumbers
+  extend ArithFn
 end
 
 PlaneFigurateNumbers = FigurateNumbers::PlaneFigurateNumbers
 SpaceFigurateNumbers = FigurateNumbers::SpaceFigurateNumbers
 MultiDimensionalFigurateNumbers = FigurateNumbers::MultiDimensionalFigurateNumbers
+ArithFn = FigurateNumbers::ArithFn
